b804d9bb5bedad9b221c89ed938ca723e89f8b60
Austin Holmes <austin.holmes4@gmail.com>
2022-12-19 13:03:00 -0700

    done up to step 7 of part 1

--- model.py
+++ model.py
@@ -1,14 +1,34 @@
 """Models for movie ratings app."""
-
+import os
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    
+    __tablename__ = "users"
 
-# Replace this with your code!
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
 
+    def __repr__(self):
+        return f"<User user_id={self.user_id} email={self.email}>"
 
-def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
+class Movie(db.Model):
+
+    __tablename__ = "movies"
+
+    movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String)
+    overview = db.Column(db.Text)
+    release_date = db.Column(db.DateTime)
+    poster_path = db.Column(db.String)
+
+    def __repr__(self):
+        return f"<Movie movie_id={self.movie_id} title={self.title}>"
+
+def connect_to_db(flask_app, db_uri=os.environ["DATABASE_URI"], echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
